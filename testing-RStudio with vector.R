58c6b8b302fc10977a3e1ba98be14cc433ffb48b
Hridita-Rubyat <hridita.rubyat@gmail.com>
2022-08-23 21:17:39 -0400

    new

---
+++ testing-RStudio with vector.R
@@ -0,0 +1,7 @@
+codes for creating vectors for c()function()
+  
+z<-c(1, 2)             #numeric
+z<-c(TRUE, FALSE)      #logical
+z<-c("x", "y", "z")    #charachter
+z<-1:10                #integer
+z<-c(1+0i, 2+1i)       #complex
